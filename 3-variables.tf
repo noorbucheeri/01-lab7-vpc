fecdda4906c3d325e9e11a43a9218d67fb8d62b3
fatima <fatimaalojaimi@hotmail.com>
2023-02-07 12:16:00 +0300

    testing

--- 3-variables.tf
+++ 3-variables.tf
@@ -1,6 +1,6 @@
 #AWS Region : us-east-1
 variable "aws_region" {
-  default = "ap-east-1"
+  default = "ap-east-1a"
 }
 
 # Environment Variable
--- 4-vpc-variables.tf
+++ 4-vpc-variables.tf
@@ -18,7 +18,7 @@ variable "vpc_cidr_block" {
 variable "vpc_availability_zones" {
   description = "LabVPC Availability Zones"
   type = list(string)
-  default = ["ap-east-1", "ap-east-1"]
+  default = ["ap-east-1a", "ap-east-1b"]
 }
 
 # VPC Public Subnets
